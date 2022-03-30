1aa85adcf2c6581547a40e6541aa100afc33b65f
JulianH99 <juliancorredor99@gmail.com>
2022-03-30 08:53:20 -0500

    feat(slider) added a neat slider on home page

--- assets/custom-slider.js
+++ assets/custom-slider.js
@@ -2,14 +2,51 @@
   const SERVER_BASE_URL = 'https://632f-190-146-238-178.ngrok.io'
 
   const fetchInfo = async () => {
-    const { length: ordersQuantity } = await fetch(`${SERVER_BASE_URL}/orders`).then(res => res.json())
-    const { length: usersQuantity } = await fetch(`${SERVER_BASE_URL}/users`).then(res => res.json())
+    const ordersElem = document.querySelector('#orders-quantity');
+    const usersElem = document.querySelector('#users-quantity');
 
-    console.log(ordersQuantity, usersQuantity)
-    document.querySelector('#orders-quantity').innerHTML = ordersQuantity;
-    document.querySelector('#users-quantity').innerHTML = usersQuantity;
+
+    try {
+
+      const { length: ordersQuantity } = await fetch(`${SERVER_BASE_URL}/orders`).then(res => res.json())
+      const { length: usersQuantity } = await fetch(`${SERVER_BASE_URL}/users`).then(res => res.json())
+
+      ordersElem.innerHTML = ordersQuantity;
+      usersElem.innerHTML = usersQuantity;
+    } catch(e) {
+      ordersElem.innerHTML = 'XXX';
+      usersElem.innerHTML = 'XXX';
+    }
+
+    
+    
   
   }
 
   fetchInfo();
+
+
+
+  const leftButton = document.querySelector('.custom-slider__arrow--left');
+  const rightButton = document.querySelector('.custom-slider__arrow--right');
+  const sliderInner = document.querySelector('.custom-slider__inner');
+  const maxSlides = getComputedStyle(
+    sliderInner
+  ).getPropertyValue('--block-number');
+
+  leftButton.addEventListener('click', function() {
+    const sliderLeft = parseInt(sliderInner.style.left.replace('%','')) || 0;
+    if(sliderLeft !== 0) {
+      sliderInner.style.left = sliderLeft + 100 + '%';
+    }
+  })
+
+  rightButton.addEventListener('click', function() {
+    const sliderLeft = parseInt(sliderInner.style.left.replace('%','') || 0);
+    if(sliderLeft !== (maxSlides-1)*(-100)) {
+      sliderInner.style.left = sliderLeft - 100 + '%';
+    }
+  })
+
+
 })();
